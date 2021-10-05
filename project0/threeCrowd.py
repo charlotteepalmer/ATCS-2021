901379bc641f1c7b5ee5bed5d24d0e3b9e85502f
Charlotte Palmer <charlotte@charlottepalmer.com>
2021-10-05 14:49:10 -0700

    If Statement Exercises

--- project0/threeCrowd.py
+++ project0/threeCrowd.py
@@ -1,5 +1,7 @@
-names = ['Charlotte', 'Jhila', 'Grace', 'Mitsuka']
-if len(names) > 3
+names = ['Charlotte', 'Jhila', 'Grace', 'Mitsuka', 'Iyanu']
+if len(names) > 3:
     print("more than 3 ppl")
 names.pop()
 names.pop()
+if len(names) > 3:
+    print("more than 3 ppl")
